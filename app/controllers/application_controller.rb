4c1bcf5342630331ef56ae8c20f6c0546e000fdb
hardow2011 <hardow2011@outlook.com>
2023-11-10 15:55:14 -0500

    wip: add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :logged_in?, :current_user
+
+  def current_user
+    user_id = session[:user_id]
+    if user_id
+      User.find(user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    redirect_to login_path unless logged_in?
+  end
 end
