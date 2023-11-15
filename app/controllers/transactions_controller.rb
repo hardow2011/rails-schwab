1a88f4bf89e45224e3fa0a7146caa6d3108f6944
hardow2011 <hardow2011@outlook.com>
2023-11-14 21:42:46 -0500

    feat: add transaction chart cache

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
   before_action :authorized
   def index
-    @transactions = current_user.get_transactions_chart_data.to_json.html_safe
+    @transactions = current_user.get_transactions_chart_data
   end
 end
--- lib/transactions_builder.rb
+++ lib/transactions_builder.rb
@@ -21,11 +21,47 @@ module TransactionsBuilder
       deposit = convert_to_float(row[5])
       running_balance = convert_to_float(row[6])
       Transaction.create(date: date, transaction_type: transaction_type, description: description,
-                      withdrawal: withdrawal, deposit: deposit,
-                      running_balance: running_balance,
-                      user: self)
+                         withdrawal: withdrawal, deposit: deposit,
+                         running_balance: running_balance,
+                         user: self)
     end
 
+    self.transactions_chart_cache = build_chart_data
+    save
+    return
+  end
+
+  def get_transactions_chart_data
+    chart_data = (transactions_chart_cache || [])
+    if chart_data.empty?
+      p "*** CACHE NOT USED. SAVE TO CACHE ***"
+      build_chart_data
+    else
+      p "*** CACHE USED. RETURN ALREADY FORMATTED DATA ***"
+      chart_data
+    end
+  end
+
+  def build_chart_data
+    chart_data = []
+    p "*** CACHE NOT USED. SAVE TO CACHE ***"
+    last_transactions_by_date = transactions.order(:created_at).group_by(&:date).transform_values(&:first).values.reverse
+    last_transactions_by_date.each_with_index do |t, i|
+      next_t = last_transactions_by_date[i + 1]
+      chart_data << t.as_json(:chart_data)
+      if next_t
+        days_between_next_transaction = (next_t.date - t.date).to_i - 1
+        if days_between_next_transaction >= 1
+          new_day = t
+          (0..days_between_next_transaction - 1).each_with_index do |_, k|
+            new_day.date = new_day.date.next_day(k + 1)
+            chart_data << new_day.as_json(:chart_data)
+            new_day.date = t.date
+          end
+        end
+      end
+    end
+    chart_data.to_json
   end
 
   private
