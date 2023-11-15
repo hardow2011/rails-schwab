93370cf237b1af3c4b8ee621c46d88ed88c26a9b
hardow2011 <hardow2011@outlook.com>
2023-11-14 22:38:26 -0500

    improve chart visuals and add avg line

--- app/javascript/transactions_chart.js
+++ app/javascript/transactions_chart.js
@@ -4,17 +4,48 @@ Chart.register(...registerables);
 
 (async function () {
     const data = transactions;
+    const avg_start = data.at(0).running_balance;
+    const avg_end = data.at(-1).running_balance;
+    const steps = (avg_end - avg_start) / data.length;
+    let avg_line = []
+
+    for (let i = 0; i <= data.length; i++) {
+        avg_line.push(avg_start + (steps * i))
+    }
 
     new Chart(
         document.getElementById('acquisitions'),
         {
             type: 'line',
+            options: {
+                fill: true,
+                elements: {
+                    point: {
+                        pointStyle: false
+                    }
+                },
+                interaction: {
+                    intersect: false,
+                    mode: 'index'
+                },
+                plugins: {
+                    tooltip: {
+                        // enabled: false
+                    }
+                }
+            },
             data: {
                 labels: data.map(row => row.date),
                 datasets: [
                     {
                         label: 'Running Balance',
                         data: data.map(row => row.running_balance)
+                    },
+                    {
+                        label: 'Average',
+                        data: avg_line,
+                        fill: false,
+                        borderDash: [10, 5]
                     }
                 ]
             }
