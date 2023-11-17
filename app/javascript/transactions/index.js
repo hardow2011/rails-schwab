8895cdeb760d09ea28cf1d21be37393f614084b7
hardow2011 <hardow2011@outlook.com>
2023-11-16 19:20:39 -0500

    feat: add range filter interactions

--- app/javascript/transactions/index.js
+++ app/javascript/transactions/index.js
@@ -16,91 +16,106 @@ function get_filtered_dates(transactions) {
         mo_3: [],
         mo_6: [],
         ytd: [],
-        year_1: [],
-        year_2: []
+        yr_1: [],
+        yr_2: []
     };
-    transactions.forEach(function(t) {
+    transactions.forEach(function (t) {
         const months = months_between_dates(new Date(), new Date(t.date));
         if (months <= 1) {
             filtered_dates.mo_1.push(t)
         }
-        if(months <= 3) {
+        if (months <= 3) {
             filtered_dates.mo_3.push(t)
         }
-        if(months <= 6) {
+        if (months <= 6) {
             filtered_dates.mo_6.push(t)
         }
         const first_date_of_year = new Date(new Date().getFullYear(), 0, 2)
-        if(months <= months_between_dates(new Date, first_date_of_year)) {
+        if (months <= months_between_dates(new Date, first_date_of_year)) {
             filtered_dates.ytd.push(t)
         }
-        if(months <= 12) {
-            filtered_dates.year_1.push(t)
+        if (months <= 12) {
+            filtered_dates.yr_1.push(t)
         }
-        if(months <= 24) {
-            filtered_dates.year_2.push(t)
+        if (months <= 24) {
+            filtered_dates.yr_2.push(t)
         }
     })
     return filtered_dates;
-    // return chart.data.filter((t) => months_between_dates(new Date(), new Date(t.date)) <= range)[0]
 }
 
 function updateChartRange(range) {
     console.log('Hola k ase')
 }
 
-(async function () {
-    const transactions = chart;
-    const filtered_dates = get_filtered_dates(transactions);
-    const avg_start = transactions.at(0).running_balance;
-    const avg_end = transactions.at(-1).running_balance;
-    const steps = (avg_end - avg_start) / transactions.length;
-    let avg_line = []
+const transactions = chart_data;
+const filtered_dates = get_filtered_dates(transactions);
 
-    for (let i = 0; i <= transactions.length; i++) {
-        avg_line.push(avg_start + (steps * i))
-    }
-
-    new Chart(
-        document.getElementById('acquisitions'),
-        {
-            type: 'line',
-            options: {
-                fill: true,
-                elements: {
-                    point: {
-                        pointStyle: false
-                    }
-                },
-                interaction: {
-                    intersect: false,
-                    mode: 'index'
-                },
-                plugins: {
-                    tooltip: {
-                        // enabled: false
-                    }
+let chart = new Chart(
+    document.getElementById('acquisitions'),
+    {
+        type: 'line',
+        options: {
+            fill: true,
+            elements: {
+                point: {
+                    pointStyle: false
                 }
             },
-            data: {
-                labels: transactions.map(row => row.date),
-                datasets: [
-                    {
-                        label: 'Running Balance',
-                        data: transactions.map(row => row.running_balance)
-                    },
-                    {
-                        label: 'Average',
-                        data: avg_line,
-                        fill: false,
-                        borderDash: [10, 5]
-                    }
-                ]
+            interaction: {
+                intersect: false,
+                mode: 'index'
+            },
+            plugins: {
+                tooltip: {
+                    // enabled: false
+                }
             }
+        },
+        data: {
+            labels: transactions.map(row => row.date),
+            datasets: [
+                {
+                    label: 'Running Balance',
+                    data: transactions.map(row => row.running_balance)
+                },
+                {
+                    label: 'Tendency',
+                    fill: false,
+                    borderDash: [10, 5]
+                }
+            ]
         }
-    );
-})();
+    }
+);
+
+drawTendencyLine(chart, transactions);
+
+function updateTransactionChart(chart, transactions, newData) {
+    chart.data.labels = [];
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = [];
+    });
+
+    chart.data.labels = newData.map(t => t.date);
+    chart.data.datasets[0].data = newData.map(t => t.running_balance);
+    drawTendencyLine(chart, newData);
+}
+
+function drawTendencyLine(chart, data) {
+    const tendency_start = data.at(0).running_balance;
+    const tendency_end = data.at(-1).running_balance;
+    const steps = (tendency_end - tendency_start) / (data.length - 1);
+    let tendency_line = []
+
+    for (let i = 0; i <= transactions.length; i++) {
+        tendency_line.push(tendency_start + (steps * i))
+    }
+
+    chart.data.datasets[1].data = tendency_line
+    chart.update();
+}
 
 window.updateChartRange = function updateChartRange(range) {
-    console.log(range)
+    updateTransactionChart(chart, transactions, filtered_dates[range])
 }
