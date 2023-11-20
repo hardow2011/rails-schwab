f3a84c6a7274919000cf8a4c743b0fcc9317af58
hardow2011 <hardow2011@outlook.com>
2023-11-20 16:43:31 -0500

    wip: login box styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,17 @@
 class SessionsController < ApplicationController
   def new
-
+    @failed_login = session[:failed_login]
+    session[:failed_login] = nil
   end
 
   def create
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
+      session[:failed_login] = nil
       session[:user_id] = @user.id
       redirect_to root_path
     else
+      session[:failed_login] = true
       redirect_to login_path
     end
   end
