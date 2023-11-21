34ab975d6b3158bc6101ae6d66f8f1c9345b2b97
hardow2011 <hardow2011@outlook.com>
2023-11-21 12:52:37 -0500

    feat: update new csv format headers

--- lib/transactions_builder.rb
+++ lib/transactions_builder.rb
@@ -8,18 +8,16 @@ module TransactionsBuilder
 
     csv = csv.values[0]
     csv = CSV.read(csv)
-    transactions_first_index = nil
-    csv.each_with_index do |row, i|
-      transactions_first_index = i + 1 if row[0] == "Posted Transactions"
-    end
+    transactions_first_index = 1
     csv[transactions_first_index..-1].each_with_index do |row, i|
+      status = row[1]
+      next if status == 'Pending'
       date = Date.strptime(row[0], "%m/%d/%Y")
-      transaction_type = TransactionType.find_or_create_by(name: row[1])
-      description = row[3]
-      puts "*** #{date} #{description}"
-      withdrawal = convert_to_float(row[4])
-      deposit = convert_to_float(row[5])
-      running_balance = convert_to_float(row[6])
+      transaction_type = TransactionType.find_or_create_by(name: row[2])
+      description = row[4]
+      withdrawal = convert_to_float(row[5])
+      deposit = convert_to_float(row[6])
+      running_balance = convert_to_float(row[7])
       Transaction.create(date: date, transaction_type: transaction_type, description: description,
                          withdrawal: withdrawal, deposit: deposit,
                          running_balance: running_balance,
