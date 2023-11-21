040b91e978f342f5821921a7e1f0ee0844374abd
hardow2011 <hardow2011@outlook.com>
2023-11-21 17:41:50 -0500

    feat: remove transaction and transactionType models

--- app/controllers/transactions_controller.rb
+++
@@ -1,6 +0,0 @@
-class TransactionsController < ApplicationController
-  before_action :authorized
-  def index
-    @transactions = current_user.get_transactions_chart_data
-  end
-end
--- db/migrate/20231110191037_create_transaction_types.rb
+++
@@ -1,9 +0,0 @@
-class CreateTransactionTypes < ActiveRecord::Migration[7.1]
-  def change
-    create_table :transaction_types do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
