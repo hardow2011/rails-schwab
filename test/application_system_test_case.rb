97386609f422ee34e7c23aa1a88bc272e4f38ae3
hardow2011 <hardow2011@outlook.com>
2023-11-23 12:19:31 -0500

    wip: user system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,13 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+
+  def login(email, password)
+    visit root_url
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+
+    click_on 'Log In'
+  end
 end
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -5,39 +5,58 @@ class UsersTest < ApplicationSystemTestCase
     @user = users(:one)
   end
 
-  test "visiting the index" do
-    visit users_url
-    assert_selector "h1", text: "Users"
+  test "should redirect to login page" do
+    visit root_url
+    assert_selector 'input[value="Log In"]'
   end
 
-  test "should create user" do
-    visit users_url
-    click_on "New user"
+  test 'should sign up new user' do
+    visit signup_url
 
-    fill_in "Email", with: @user.email
-    fill_in "Password digest", with: @user.password_digest
-    click_on "Create User"
+    fill_in 'Email', with: 'qwerty@yahoo.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
 
-    assert_text "User was successfully created"
-    click_on "Back"
+    click_on 'Sign Up'
+
+    assert_text 'Summary'
+    assert_text '$0'
+    assert_text 'Total Value'
   end
 
-  test "should update User" do
-    visit user_url(@user)
-    click_on "Edit this user", match: :first
-
-    fill_in "Email", with: @user.email
-    fill_in "Password digest", with: @user.password_digest
-    click_on "Update User"
-
-    assert_text "User was successfully updated"
-    click_on "Back"
+  test 'should redirect to transactions chart after logging in existing user' do
+    login(@user.email, 'cajeipracrafo')
+    assert_text 'Summary'
   end
 
-  test "should destroy User" do
-    visit user_url(@user)
-    click_on "Destroy this user", match: :first
+  # test "should create user" do
+  #   visit users_url
+  #   click_on "New user"
+  #
+  #   fill_in "Email", with: @user.email
+  #   fill_in "Password digest", with: @user.password_digest
+  #   click_on "Create User"
+  #
+  #   assert_text "User was successfully created"
+  #   click_on "Back"
+  # end
 
-    assert_text "User was successfully destroyed"
-  end
+  # test "should update User" do
+  #   visit user_url(@user)
+  #   click_on "Edit this user", match: :first
+  #
+  #   fill_in "Email", with: @user.email
+  #   fill_in "Password digest", with: @user.password_digest
+  #   click_on "Update User"
+  #
+  #   assert_text "User was successfully updated"
+  #   click_on "Back"
+  # end
+
+  # test "should destroy User" do
+  #   visit user_url(@user)
+  #   click_on "Destroy this user", match: :first
+  #
+  #   assert_text "User was successfully destroyed"
+  # end
 end
