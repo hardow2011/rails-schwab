dd33accc64a431b3b52792a26ca7d6ab657396f5
hardow2011 <hardow2011@outlook.com>
2023-11-24 22:01:21 -0500

    feat: create test for transactions csv setting correct amount of transactions in user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,6 +15,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @test_csv_path = 'test/fixtures/files/test_csv.csv'
+  end
 
   test "should not save user without email" do
     user = User.new(password: 'mypassword')
@@ -30,6 +33,13 @@ class UserTest < ActiveSupport::TestCase
   test "should not save user without password" do
     user = User.new(email: 'deo@njode.com')
     assert_not user.save, 'Saved the user without a password'
+  end
+
+  test "transactions csv should set correct amount of transactions" do
+    csv = File.open(@test_csv_path)
+    user = users(:one)
+    user.set_transactions(csv)
+    assert_equal 64, user.transactions.size, 'Transaction csv should have created 64 transactions'
   end
 
 end
