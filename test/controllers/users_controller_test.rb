a867b42cc41ea9daea43244f99fae2e3d086286c
hardow2011 <hardow2011@outlook.com>
2023-11-26 10:51:59 -0500

    wip: user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,46 +3,62 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    @user_password = 'cajeipracrafo'
   end
 
-  test "should get index" do
+  test "index should not be found" do
     get users_url
-    assert_response :success
+    assert_response :not_found
   end
 
-  test "should get new" do
-    get new_user_url
+  test "should get login" do
+    get login_url
     assert_response :success
+    sign_in_as(@user.email, @user_password)
+    get login_url
+    assert_redirected_to root_url, message = 'Should have redirected to root after login'
+  end
+
+  test "should get signup" do
+    get signup_url
+    assert_response :success
+    sign_in_as(@user.email, @user_password)
+    get signup_url
+    assert_redirected_to root_url, message = 'Should have redirected to root afetr login'
   end
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email: @user.email, password_digest: @user.password_digest } }
+      post users_url, params: { user: { email: 'ola@k.ase', password: 'saludos', password_confirmation: 'saludos' } }
     end
 
-    assert_redirected_to user_url(User.last)
+    assert_redirected_to root_url
   end
 
   test "should show user" do
-    get user_url(@user)
-    assert_response :success
+    get user_url
+    assert_redirected_to login_url, message = 'Should have redirected to login page'
+    sign_in_as(@user.email, @user_password)
+    assert_redirected_to root_url, message = 'Should have redirected to root page'
+    get user_url
+    assert_response :success, 'Should not have show user without login'
   end
 
-  test "should get edit" do
-    get edit_user_url(@user)
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, password_digest: @user.password_digest } }
-    assert_redirected_to user_url(@user)
-  end
-
-  test "should destroy user" do
-    assert_difference("User.count", -1) do
-      delete user_url(@user)
-    end
-
-    assert_redirected_to users_url
-  end
+  # test "should get edit" do
+  #   get edit_user_url(@user)
+  #   assert_response :success
+  # end
+  #
+  # test "should update user" do
+  #   patch user_url(@user), params: { user: { email: @user.email, password_digest: @user.password_digest } }
+  #   assert_redirected_to user_url(@user)
+  # end
+  #
+  # test "should destroy user" do
+  #   assert_difference("User.count", -1) do
+  #     delete user_url(@user)
+  #   end
+  #
+  #   assert_redirected_to users_url
+  # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,14 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    module SignInHelper
+      def sign_in_as(user, password)
+        post sessions_url, params: { email: @user.email, password: @user_password }
+      end
+    end
+
+    class ActionDispatch::IntegrationTest
+      include SignInHelper
+    end
   end
 end
