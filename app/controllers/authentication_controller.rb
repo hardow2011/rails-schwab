098deb5a7288602f275b070992d5768087aadfc0
hardow2011 <hardow2011@outlook.com>
2023-11-27 21:53:37 -0500

    feat: generate authentication controller

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,8 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
+  def create
+    @user = User.find_or_create_by(email: params[:email])
+    user.send_magic_link
+  end
+end
