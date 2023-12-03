0f56b153d788dd80babc44fd3c9ed9fbc90ac7d3
hardow2011 <hardow2011@outlook.com>
2023-12-02 20:23:09 -0500

    feat: create rescue for expired token and add flash to layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,26 @@ class SessionsController < ApplicationController
 
   def create
     login_token = params[:login_token]
-    decoded_token = JsonWebToken.decode(login_token)
+    begin
+      decoded_token = JsonWebToken.decode(login_token)
+    rescue JWT::ExpiredSignature
+      # TODO: add expired token flash redirect
+      flash[:alert] = ['Expired token']
+      redirect_to root_path
+    end
 
     if decoded_token && JsonWebToken.valid_payload(decoded_token.first)
       user = User.find_by(login_token: login_token)
       if user
-        # If user is unregistered, that is, signing up, then registered it
+        # If user is unregistered, that is, signing up. Then registered it
         unless user.registered?
           user.registered = true
           user.save
         end
           session[:auth_token] = user.generate_auth_token
           redirect_to root_path
-      # TODO: add else
+      else
+        # TODO: add invalid token flash redirect
       end
     end
   end
