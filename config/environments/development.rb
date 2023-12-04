bc5d277c9907eb9b91cce9cfcf560629472473ec
hardow2011 <hardow2011@outlook.com>
2023-12-04 15:27:59 -0500

    feat: set default host for Rails.application.routes.url_helpers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # TODO: add Rails.application.routes.default_url_options[:host] for other envs
+  # Set default host for Rails.application.routes.url_helpers
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
