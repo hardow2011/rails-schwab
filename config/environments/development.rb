bae63446084d609bc8ce152cd9adb8d387ee2845
hardow2011 <hardow2011@outlook.com>
2023-12-15 14:03:12 -0500

    refactor: added TODO

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # TODO: configure mailer for other envs
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
