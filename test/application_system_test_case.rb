057890195a0e5264a0eaea6ac21a97aa712b5178
hardow2011 <hardow2011@outlook.com>
2023-12-16 22:41:48 -0500

    refactor: remove test function

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -8,12 +8,4 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     Rails.application.routes.default_url_options[:host] = Capybara.current_session.server.host
     Rails.application.routes.default_url_options[:port] = Capybara.current_session.server.port
   end
-
-  def login(email, password)
-    visit root_url
-    fill_in 'Email', with: email
-    fill_in 'Password', with: password
-
-    click_on 'Log In'
-  end
 end
