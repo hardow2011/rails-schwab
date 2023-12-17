ec1bcbd9feaab28c48e4f3952912451abbe0488b
hardow2011 <hardow2011@outlook.com>
2023-12-16 22:17:26 -0500

    fix: set Rails host and port to Capybara's host and port when running system tests, so that the emails urls can be accessed

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,6 +3,11 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 
+  setup do
+    # Make sure to set Rails host and port to whatever Capybara's host and port are when running the system tests
+    Rails.application.routes.default_url_options[:host] = Capybara.current_session.server.host
+    Rails.application.routes.default_url_options[:port] = Capybara.current_session.server.port
+  end
 
   def login(email, password)
     visit root_url
