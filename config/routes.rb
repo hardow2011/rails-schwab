18e36cbe75a3e3867c80ad948503f53d45360d45
hardow2011 <hardow2011@outlook.com>
2023-12-21 18:29:10 -0500

    feat: added routes for deleting user

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   patch '/user', to: 'users#update'
   post '/request_destroy', to: 'users#request_destroy'
+  post '/destroy_user', to: 'users#destroy'
   get '/request_email_change', to: 'users#request_email_change'
   get '/user', to: 'users#show'
   patch '/update_email', to: 'users#update_email'
@@ -23,6 +24,7 @@ Rails.application.routes.draw do
   match 'sessions', to: 'sessions#create', via: :get
   delete '/sessions', to: 'sessions#destroy'
   get '/change_email', to: 'sessions#change_email'
+  get '/process_user_destroy_request', to: 'sessions#process_user_destroy_request'
 
   post '/transactions_csv', to: 'users#transactions_csv'
   get '/transactions_json', to: 'users#transactions_json'
