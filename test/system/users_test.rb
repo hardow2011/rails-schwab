c80b9aaf4f25eb1e3100d0c20b1297f99c797ec9
hardow2011 <hardow2011@outlook.com>
2023-12-21 12:19:01 -0500

    feat: added system tests

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,62 +1,95 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
   setup do
     @user = users(:one)
   end
 
-  test "should redirect to login page" do
-    visit root_url
-    assert_selector 'input[value="Log In"]'
+  test "should login user" do
+    login(@user)
   end
 
   test 'should sign up new user' do
     visit signup_url
 
-    fill_in 'Email', with: 'qwerty@yahoo.com'
-    fill_in 'Password', with: '123456'
-    fill_in 'Password confirmation', with: '123456'
+    assert_selector 'input[value="Sign Up"]'
 
-    click_on 'Sign Up'
+    fill_in 'Email', with: 'qwerty@yahoo.com'
+
+    perform_enqueued_jobs do
+      click_on 'Sign Up'
+    end
+
+    assert_text 'Signup email sent. Please check your inbox'
+
+    assert_selector 'input[value="Log In"]'
+
+    visit get_job_link(enqueued_jobs.last)
 
     assert_text 'Summary'
     assert_text '$0'
     assert_text 'Total Value'
   end
 
-  test 'should redirect to transactions chart after logging in existing user' do
-    login(@user.email, 'cajeipracrafo')
-    assert_text 'Summary'
+  test 'should redirect to user page' do
+    visit user_url
+
+    assert_selector 'input[value="Log In"]'
+
+    fill_in 'Email', with: @user.email
+
+    perform_enqueued_jobs do
+      click_on 'Log In'
+    end
+
+    assert_text 'Login email sent. Please check your inbox'
+
+    visit get_job_link(enqueued_jobs.last)
+
+    assert_text 'You currently have 0 transactions in total.'
+
+    assert_selector 'a[href="/request_email_change"]'
+    assert_selector 'input[type="submit"][value="Upload Transactions CSV"]'
   end
 
-  # test "should create user" do
-  #   visit users_url
-  #   click_on "New user"
-  #
-  #   fill_in "Email", with: @user.email
-  #   fill_in "Password digest", with: @user.password_digest
-  #   click_on "Create User"
-  #
-  #   assert_text "User was successfully created"
-  #   click_on "Back"
-  # end
+  test "should update User" do
+    login(@user)
 
-  # test "should update User" do
-  #   visit user_url(@user)
-  #   click_on "Edit this user", match: :first
-  #
-  #   fill_in "Email", with: @user.email
-  #   fill_in "Password digest", with: @user.password_digest
-  #   click_on "Update User"
-  #
-  #   assert_text "User was successfully updated"
-  #   click_on "Back"
-  # end
+    visit user_url
 
-  # test "should destroy User" do
-  #   visit user_url(@user)
-  #   click_on "Destroy this user", match: :first
-  #
-  #   assert_text "User was successfully destroyed"
-  # end
+    assert_text 'You currently have 0 transactions in total.'
+
+    assert_selector 'a[href="/request_email_change"]'
+    assert_selector 'input[type="submit"][value="Upload Transactions CSV"]'
+
+    perform_enqueued_jobs do
+      click_on 'Change Email'
+    end
+
+    # Without the sleep, the email change job doesn't get added in time for visit get_job_link(enqueued_jobs.last).
+    # Not sure why
+    sleep 0.1
+
+    visit get_job_link(enqueued_jobs.last)
+
+    fill_in 'user[new_email]', with: 'new@email.com'
+    fill_in 'user[new_email_confirmation]', with: 'new@email.com'
+
+    perform_enqueued_jobs do
+      click_on 'Change Email'
+    end
+
+    # Without the sleep, the email change job doesn't get added in time for visit get_job_link(enqueued_jobs.last).
+    # Not sure why
+    sleep 0.1
+
+    visit get_job_link(enqueued_jobs.last)
+
+    assert_text 'Email updated successfully'
+
+    assert_text 'Summary'
+    assert_text '$0'
+    assert_text 'Total Value'
+  end
 end
