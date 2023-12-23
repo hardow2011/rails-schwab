1590b52c6d571877e5c741bb57c7ace53f038fe4
hardow2011 <hardow2011@outlook.com>
2023-12-22 21:34:23 -0500

    refactor: add user to test setup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,6 +22,7 @@ class UserTest < ActiveSupport::TestCase
   setup do
     @test_csv_path = 'test/fixtures/files/test_csv.csv'
     @test_csv_2_path = 'test/fixtures/files/test_csv_2.csv'
+    @user = users(:one)
   end
 
   test "should not save user without email" do
@@ -43,33 +44,30 @@ class UserTest < ActiveSupport::TestCase
 
   test "transactions csv should set correct amount of transactions" do
     csv = File.open(@test_csv_path)
-    user = users(:one)
-    user.set_transactions(csv)
-    assert_equal 64, user.transactions.size, 'Transaction csv should have created 64 transactions'
+    @user.set_transactions(csv)
+    assert_equal 64, @user.transactions.size, 'Transaction csv should have created 64 transactions'
   end
 
   test "second transactions csv should overwrite first transactions csv" do
     csv_1 = File.open(@test_csv_path)
     csv_2 = File.open(@test_csv_2_path)
-    user = users(:one)
 
-    user.set_transactions(csv_1)
-    assert_equal 64, user.transactions.size, 'First transaction csv should have created 64 transactions'
+    @user.set_transactions(csv_1)
+    assert_equal 64, @user.transactions.size, 'First transaction csv should have created 64 transactions'
 
-    user.set_transactions(csv_2)
-    assert_equal 24, user.transactions.size, 'Second transaction csv should have overwritten transactions to 24'
+    @user.set_transactions(csv_2)
+    assert_equal 24, @user.transactions.size, 'Second transaction csv should have overwritten transactions to 24'
   end
 
   test "transactions chart data should return a correct amount of days " do
     csv_1 = File.open(@test_csv_path)
     csv_2 = File.open(@test_csv_2_path)
-    user = users(:one)
 
-    user.set_transactions(csv_1)
-    assert_equal 43, JSON.parse(user.get_transactions_chart_data).size, 'First transactions csv should have returned a chart data of 43 days'
+    @user.set_transactions(csv_1)
+    assert_equal 43, JSON.parse(@user.get_transactions_chart_data).size, 'First transactions csv should have returned a chart data of 43 days'
 
-    user.set_transactions(csv_2)
-    assert_equal 22, JSON.parse(user.get_transactions_chart_data).size, 'Second transactions csv should have returned a chart data of 22 days'
+    @user.set_transactions(csv_2)
+    assert_equal 22, JSON.parse(@user.get_transactions_chart_data).size, 'Second transactions csv should have returned a chart data of 22 days'
   end
 
 end
