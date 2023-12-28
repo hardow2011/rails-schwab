e0c87d336e1662a671ec7286936d5f6ab5944d1a
hardow2011 <hardow2011@outlook.com>
2023-12-28 13:25:46 -0500

    feat: init sentry file

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,13 @@
+Sentry.init do |config|
+  config.dsn = 'https://a9feaf8e6fb9402cd5672d84840de822@o4506474750869504.ingest.sentry.io/4506474753490944'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
