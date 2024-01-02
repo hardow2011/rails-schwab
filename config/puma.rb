f05df54375ff6c0d7963003a226ca1dab575ab90
hardow2011 <hardow2011@outlook.com>
2024-01-02 11:24:17 -0500

    wip: testing puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,56 +1,20 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# config/puma.rb
+environment ENV.fetch('RAILS_ENV') { 'production' }
+daemonize true
 
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
-
-# Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production" or ENV["RAILS_ENV"] == "staging"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
-end
-
-app_dir = File.expand_path("../..", __FILE__)
+app_dir = File.expand_path('../..', __FILE__)
 shared_dir = "#{app_dir}/shared"
 
-# Set up socket location
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+bind "unix://#{shared_dir}/sockets/puma.sock"
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+stdout_redirect "#{shared_dir}/log/puma_access.log", "#{shared_dir}/log/puma_error.log", true
 
-# Logging
-stdout_redirect "#{shared_dir}/tmp/log/puma.stdout.log", "#{shared_dir}/tmp/log/puma.stderr.log", true
+workers 1 # Adjust as per your server capacity
+threads 1, 6
 
-# Set master PID and state locations
-# pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/tmp/pids/puma.state"
-activate_control_app
-
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "#{shared_dir}/tmp/pids/server.pid" }
-
-# Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
+preload_app!
 
 on_worker_boot do
-  require "active_record"
-  # ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  # ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
