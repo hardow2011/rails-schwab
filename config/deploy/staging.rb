891a095fc9f908613d2389c934915d45a8f50050
hardow2011 <hardow2011@outlook.com>
2024-01-12 14:51:08 -0500

    fix: moved capistrano staging server ip out of credential file

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server Rails.application.credentials.capistrano.staging.server_ip, port: 22, roles: [:web, :app, :db], primary: true
+server '18.117.154.120', port: 22, roles: [:web, :app, :db], primary: true
 
 set :branch, :staging
 
