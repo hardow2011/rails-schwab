d813a115dcbd0d12b994dcea241350a0562a6ea1
hardow2011 <hardow2011@outlook.com>
2024-01-27 15:22:39 -0500

    fix: correct server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "18.223.16.92", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "172.31.38.112", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
